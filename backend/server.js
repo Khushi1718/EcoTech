59bf36ee525bfa21f7d7f41ccf56c7fe955d69c3
Khushi <khushi@Khushis-MacBook-Air.local>
2026-04-12 13:07:09 +0530

    corrected auth.js file in frontend due to signup issue

--- backend/server.js
+++ backend/server.js
@@ -73,7 +73,14 @@ const app = express();
 const PORT = process.env.PORT || 5001;
 
 // CORS
-app.use(cors());
+// app.use(cors());
+app.use(cors({
+  origin: "https://eco-tech-8a1a.vercel.app",
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  credentials: true
+}));
+
+app.options("*", cors()); // 🔥 VERY IMPORTANT
 // Middleware
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ limit: "10mb", extended: true }));
